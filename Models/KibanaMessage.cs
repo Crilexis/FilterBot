f5d40edc4213624903437d2c1b65102b34e102e2
Vinicius Langbehn <vinicius.langbehn@volvo.com>
2021-10-21 12:27:59 -0300

    First commit

---
+++ Models/KibanaMessage.cs
@@ -0,0 +1,95 @@
+﻿
+using System;
+
+public class KibanaMessage
+{
+    public int took { get; set; }
+    public bool timed_out { get; set; }
+    public _Shards _shards { get; set; }
+    public Hits hits { get; set; }
+    public Aggregations aggregations { get; set; }
+    public int status { get; set; }
+}
+
+public class _Shards
+{
+    public int total { get; set; }
+    public int successful { get; set; }
+    public int skipped { get; set; }
+    public int failed { get; set; }
+}
+
+public class Hits
+{
+    public int total { get; set; }
+    public object max_score { get; set; }
+    public Hit[] hits { get; set; }
+}
+
+public class Hit
+{
+    public string _index { get; set; }
+    public string _type { get; set; }
+    public string _id { get; set; }
+    public int _version { get; set; }
+    public object _score { get; set; }
+    public _Source _source { get; set; }
+    public Fields1 fields { get; set; }
+    public Highlight highlight { get; set; }
+    public long[] sort { get; set; }
+}
+
+public class _Source
+{
+    public DateTime timestamp { get; set; }
+    public string level { get; set; }
+    public string messageTemplate { get; set; }
+    public string message { get; set; }
+    public Exceptions[] exceptions { get; set; }
+    public Fields fields { get; set; }
+}
+
+public class Fields
+{
+    public string SourceContext { get; set; }
+}
+
+public class Exceptions
+{
+    public int Depth { get; set; }
+    public string ClassName { get; set; }
+    public string Message { get; set; }
+    public string Source { get; set; }
+    public string StackTraceString { get; set; }
+    public object RemoteStackTraceString { get; set; }
+    public int RemoteStackIndex { get; set; }
+    public int HResult { get; set; }
+    public object HelpURL { get; set; }
+}
+
+public class Fields1
+{
+    public DateTime[] timestamp { get; set; }
+}
+
+public class Highlight
+{
+    public string[] message { get; set; }
+}
+
+public class Aggregations
+{
+    public _2 _2 { get; set; }
+}
+
+public class _2
+{
+    public Bucket[] buckets { get; set; }
+}
+
+public class Bucket
+{
+    public DateTime key_as_string { get; set; }
+    public long key { get; set; }
+    public int doc_count { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace BotFilterData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inform file to load");
+            string file = Console.ReadLine();
+
+            if (!File.Exists(file))
+                Console.WriteLine($"File {file} not found");
+
+            Console.WriteLine($"{Environment.NewLine}Inform operation type: {Environment.NewLine}1. Kibana message dump");
+
+            if (!int.TryParse(Console.ReadLine(), out int option))
+                Console.WriteLine("Invalid option");
+
+            try
+            {
+                string json = File.ReadAllText(file);
+
+                Action<string> action = option switch
+                {
+                    1 => WriteKibanaMassage,
+                    _ => x => Console.WriteLine($"{Environment.NewLine}Invalid option selected"),
+                };
+
+                action(json);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{Environment.NewLine}Invalid operation: \r\n{e.Message}");
+            }
+
+            Console.WriteLine($"{Environment.NewLine}Processing done");
+            Console.ReadLine();
+        }
+
+        static void WriteKibanaMassage(string json)
+        {
+            KibanaMessage messageDump = JsonSerializer.Deserialize<KibanaMessage>(json);
+            if (messageDump == null)
+                throw new Exception("Invalid json");                
+
+            Console.WriteLine($"{Environment.NewLine}Inform RegEx to be applyied to the message surrounded by quotes");
+
+            string pattern = Console.ReadLine().Trim('"');
+            Regex regex = new Regex(pattern);
+
+            if (regex == null)
+                throw new Exception("Invalid pattern");
+
+            List<string> messages = messageDump.hits.hits.Select(x => regex.Match(x._source.message).Groups[1].Value).ToList();
+
+            if(messages.Count > 0)
+            {
+                File.WriteAllLines("output.txt", messages);
+            }
+
+            Console.WriteLine($"{Environment.NewLine}Data wrote to output.txt");
+        }
+    }
+}
